974152d0f535c0deeb4bb832f5bdfde247ad70e6
Ryan Zebian <ryan.zebian@padi.com>
2018-07-05 14:47:40 -0700

    add find-users

--- find-users/handler.js
+++ find-users/handler.js
@@ -2,32 +2,27 @@
 
 const MongoClient = require('mongodb').MongoClient
 
-var clientsDB;  // Cached connection-pool for further requests.
+var clientsDB; // Cached connection-pool for further requests.
 
 module.exports = (event, context) => {
     prepareDB()
-    .then((users) => {
-        const record = {"name": event.body };
-
-        users.collection("users").insertOne(record, (insertErr) => {
-            if(insertErr) {
-                return context.fail(insertErr.toString());
-            }
-
-            const result =  {
-                status: "Insert done of: " + JSON.stringify(event.body)
+        .then((users) => {
+            const record = {
+                "name": event.body
             };
-    
-            context
-                .status(200)
-                .succeed(result);
+            users.collection("users")
+                .find().limit(50).toArray((err, docs) => {
+                    if (err) {
+                        return context.fail(err.toString());
+                    }
+                    context
+                        .status(200)
+                        .succeed(docs)
+                });
+        }).catch(err => {
+            context.fail(err.toString());
         });
-    })
-    .catch(err => {
-        context.fail(err.toString());
-    });
 }
-
 const prepareDB = () => {
     const url = "mongodb://" + process.env.mongo + ":27017/clients"
 
